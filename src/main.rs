52ddcf595c0a3c2bbc9a58d5494489fbfa52b9e3
Ebooth <pauldejeandev@gmail.com>
2025-02-23 03:48:37 +0100

    [Step 1] Display the first line of traceroute command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::Parser;
+use std::process;
+use traceroute::{execute_command, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = Args::parse();
+    let error_code = execute_command(&mut args);
+    process::exit(error_code);
 }
